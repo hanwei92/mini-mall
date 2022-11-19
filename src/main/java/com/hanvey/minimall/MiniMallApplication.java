9b69d25a93f760eb37dde13409faf5252f85204c
zhuhanwei <706925833@qq.com>
2022-11-19 11:21:54 +0800

    init

---
+++ src/main/java/com/hanvey/minimall/MiniMallApplication.java
@@ -0,0 +1,13 @@
+package com.hanvey.minimall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniMallApplication.class, args);
+    }
+
+}
